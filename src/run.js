ffb0e43f4ee3e6afa7e6c63cf2db8d5cc96b5851
SlowShip <Joshua.Asprey@gmail.com>
2016-07-14 15:02:04 +0100

    Merged argument parsing back into main method. Clearer whats going on now imo

--- src/run.js
+++ src/run.js
@@ -7,42 +7,34 @@ const url = require('url');
 const log = require('./log');
 const register = require('./register');
 
-function gatherVaultArguments(hostURI) {
-  const parsedURI = url.parse(hostURI);
-  if(!parsedURI || !parsedURI.protocol || !parsedURI.host) {
-    throw new Error('Bad input: could not parse host');
-  }
-
-  const vaultAuth = parsedURI.auth.split(':');
-  if(!vaultAuth[0] || !vaultAuth[1]) {
-    throw new Error('Bad input: user authentication was in a invalid format')
-  }
-
-  return {
-    url: `${parsedURI.protocol}//${parsedURI.host}`,
-    auth: {
-      username: vaultAuth[0],
-      password: vaultAuth[1]
-    }
-  }
-}
-
 module.exports = function(hostURI, appId) {
   if(!hostURI) {
     throw new Error('Bad input: no host supplied');
   }
-
   if(!appId) {
     throw new Error('Bad input: no appId supplied');
   }
 
-  const userId = uuid.v4();
-  const vaultArgs = gatherVaultArguments(hostURI);
+  const parsedURI = url.parse(hostURI);
+  if(!parsedURI || !parsedURI.protocol || !parsedURI.host) {
+    throw new Error('Bad input: could not parse host');
+  }
   const vault = new Vault({
-    url: vaultArgs.url
+    url: `${parsedURI.protocol}//${parsedURI.host}`
   });
 
-  register(vault, vaultArgs.auth, appId, userId, (error) => {
+  const authParts = parsedURI.auth.split(':');
+  if(!authParts[0] || !authParts[1]) {
+    throw new Error('Bad input: user authentication was in a invalid format')
+  }
+  const vaultAuth = {
+    username: authParts[0],
+    password: authParts[1]
+  };
+
+  const userId = uuid.v4();
+
+  register(vault, vaultAuth, appId, userId, (error) => {
     if(error) {
       throw error;
     }
