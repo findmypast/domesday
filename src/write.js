8651e3f38916ad73a6b0905bb83f1150f62d0088
James Ford <jford@findmypast.com>
2017-04-19 15:00:24 +0100

    further testing of awesome new functionality in domesday

--- src/write.js
+++ src/write.js
@@ -1,9 +1,19 @@
 'use strict';
 
+const Promise = require('bluebird');
+
 module.exports = (vaultDetails, key, value) => {
   const vault = vaultDetails.vaultClient;
   const vaultAuth = vaultDetails.vaultAuth;
   const authenticator = vaultDetails.vaultAuthenticator;
+
+  if(!key) {
+    Promise.reject('You must supply a key');
+  }
+
+  if(!value) {
+    Promise.reject('You must supply a value');
+  }
 
   return authenticator(vaultAuth)
   .then( result => {
