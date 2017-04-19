2dff9a2b03685b886f32136af51445748eec0bc6
Mike Thomas <mthomas@findmypast.com>
2017-04-19 12:21:53 +0100

    Remove console.log statement - affecting the builds

--- src/write.js
+++ src/write.js
@@ -7,7 +7,6 @@ module.exports = (vault, vaultAuth, key, value) => {
     return vault.userpassLogin(vaultAuth)
     .then( result => {
       vault.token = result.auth.client_token;
-      console.log(value);
       return vault.write(key, value);
     });
 }
